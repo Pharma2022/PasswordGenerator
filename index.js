c43c295804d066f5990db1a3d38b53033aa81e1c
Pharma2022 <111680056+Pharma2022@users.noreply.github.com>
2022-12-29 11:04:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ let  characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 const symbolsArray=["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?"];
 const  numbersArray= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let passwordLength= 15;
+const numberEl= document.getElementById("numbers")
 
+const  symbolEl= document.getElementById("symbols")
+
+const firstPass = document.getElementById("firstNewPass-el")
+const secondPass = document.getElementById("secondNewPass-el")
 /* event listeners*/
 document.addEventListener("click",e=>{
     e.preventDefault()
-    e.target.id === "numbers"?  toggleCharacter(document.getElementById("numbers"),"Numbers",numbersArray,1):
-    e.target.id === "symbols"? toggleCharacter(document.getElementById("symbols"),"Symbols",symbolsArray,"?"):
-    e.target.id === "generate-el"? generate("firstNewPass-el","secondNewPass-el"):""
+    e.target.id === "numbers"?  toggleCharacter(numberEl,"Numbers",numbersArray,1):
+    e.target.id === "symbols"? toggleCharacter(symbolEl,"Symbols",symbolsArray,"?"):
+    e.target.id === "generate-el"? generate(firstPass,secondPass):""
   
     })
 
-document.getElementById("firstNewPass-el").addEventListener("click", async ()=>await navigator.clipboard.writeText(document.getElementById("firstNewPass-el").innerText))
-document.getElementById("secondNewPass-el").addEventListener("click", async ()=>await navigator.clipboard.writeText(document.getElementById("secondNewPass-el").innerText))
+firstPass.addEventListener("click", async ()=>await navigator.clipboard.writeText(firstPass).innerText)
+secondPass.addEventListener("click", async ()=>await navigator.clipboard.writeText(secoondPass).innerText)
+
 
 document.getElementById("passwordLength").addEventListener("change",()=>passwordLength=document.getElementById("passwordLength").value)
 
@@ -28,15 +34,13 @@ characterel.innerText= characterel.innerText === "Add "+numberSymbolHtml?
      "Remove "+numberSymbolHtml :"Add "+numberSymbolHtml  }
 
 const generate =  (firstId,secondId) =>{
-document.getElementById(`${firstId}`).textContent=""
-document.getElementById(`${secondId}`).textContent=""
+firstId.textContent=""
+secondId.textContent=""
     
     for (let i=0; i< passwordLength; i++)
-    {document.getElementById(`${firstId}`).textContent+= characters[randomNumber(characters)]
-    document.getElementById(`${secondId}`).textContent+= characters[randomNumber(characters)]
+    {firstId.textContent+= characters[randomNumber(characters)]
+    secondId.textContent+= characters[randomNumber(characters)]
      }
   }
   
   const randomNumber =  myCharacters => Math.floor(Math.random()*myCharacters.length)
-
-
