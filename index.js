ab7a56e325426b49bacea6c6f31c20d62ddb36b0
Pharma2022 <111680056+Pharma2022@users.noreply.github.com>
2022-12-29 14:02:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ let  characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 const symbolsArray=["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?"]
 const  numbersArray= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 let passwordLength= 15
+
 const numberEl= document.getElementById("numbers")
-
 const  symbolEl= document.getElementById("symbols")
-
 const firstPass = document.getElementById("firstNewPass-el")
 const secondPass = document.getElementById("secondNewPass-el")
+
 /* event listeners*/
 document.addEventListener("click",e=>{
     e.preventDefault()
@@ -16,8 +16,9 @@ document.addEventListener("click",e=>{
     e.target.id === "generate-el"? generate(firstPass,secondPass):""
   
     })
-document.getElementById("firstNewPass-el").addEventListener("click", async ()=>await navigator.clipboard.writeText(document.getElementById("firstNewPass-el").innerText))
-document.getElementById("secondNewPass-el").addEventListener("click", async ()=>await navigator.clipboard.writeText(document.getElementById("secondNewPass-el").innerText))
+
+firstPass.addEventListener("click", async ()=>await navigator.clipboard.writeText(firstPass.innerText))
+secondPass.addEventListener("click", async ()=>await navigator.clipboard.writeText(secondPass.innerText))
 
 document.getElementById("passwordLength").addEventListener("change",()=>passwordLength=document.getElementById("passwordLength").value)
 
